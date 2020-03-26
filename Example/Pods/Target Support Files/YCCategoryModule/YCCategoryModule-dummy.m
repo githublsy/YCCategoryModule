ff01a6a2eee0edc9e7f05662a9f236008e946180
githublsy <oyekissliu@163.com>
2020-03-26 21:01:37 +0800

    firstT

---
+++ Example/Pods/Target Support Files/YCCategoryModule/YCCategoryModule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YCCategoryModule : NSObject
+@end
+@implementation PodsDummy_YCCategoryModule
+@end
---
+++ YCCategoryModule/Classes/UIView+SYExtension.h
@@ -0,0 +1,25 @@
+//
+//  UIView+SYExtension.h
+//  LFCApp
+//
+//  Created by Lssyyy on 2019/2/27.
+//  Copyright © 2019年 Lssyyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (SYExtension)
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (assign, nonatomic) CGFloat left;
+@property (assign, nonatomic) CGFloat right;
+@property (assign, nonatomic) CGFloat top;
+@property (assign, nonatomic) CGFloat bottom;
+@property (assign, nonatomic) CGFloat width;
+@property (assign, nonatomic) CGFloat height;
+@property (assign, nonatomic) CGFloat centerX;
+@property (assign, nonatomic) CGFloat centerY;
+@property (assign, nonatomic) CGSize size;
+@property (assign, nonatomic) CGPoint origin;
+@end
+
